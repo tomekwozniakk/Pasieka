4d943241d5878446bca6716cbeff7eeb844ef18d
Tomek Wozniak <tomekwozniakk@gmail.com>
2020-01-30 15:10:47 +0100

    added popups on about.html

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-const $burger = $("button.burger")
-const $nav = $("nav")
-const $icon = $(".burger i")
+const $burger = $("button.burger");
+const $nav = $("nav");
+const $icon = $(".burger i");
 
 $burger.on("click", function() {
     $icon.toggleClass("active")
     $nav.toggleClass("active")
+});
+
+$("section").on("click", function() {
+    $(".popup_wrap, popup_wrap div").addClass("active");
+    $("div.wrap").addClass("blur");
+});
+
+$("button.close").on("click", function() {
+    $(".popup_wrap, .popup_wrap div").removeClass("active");
+    $("div.wrap").removeClass("blur");
+    $this.removeClass("active")
 })
+
+$(".one").on("click", function() {
+    $(".popup_one, .popup_one button").addClass("active");
+});
+
+$(".two").on("click", function() {
+    $(".popup_two, .popup_two button").addClass("active");
+});
+
+$(".three").on("click", function() {
+    $(".popup_three, .popup_three button").addClass("active");
+});
+
+$(".four").on("click", function() {
+    $(".popup_four, .popup_four button").addClass("active");
+});
